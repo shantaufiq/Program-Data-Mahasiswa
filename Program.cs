dcdeaf31e6aa7fb3f752aa7f239550743acb2b7c
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2021-06-19 08:23:35 +0800

    percobaan 1

--- Program.cs
+++ Program.cs
@@ -6,77 +6,231 @@ namespace Project_Assessment_3
     {
         struct Mahasiswa
 		{
-			public string Absen;
+			public string Kelamin;
 			public string Nama;
-			public string NIM;
-		}
-        static void Main(string[] args)
+			public int Id;
+            
+		}        
+		
+        static Mahasiswa[] MyGlobal = new Mahasiswa[10]; //variabel array gelobal
+        
+        int input; //mengecheck jumlah length variabel array gelobal
+        int urutan = 1; //menentukan urutan index
+        
+        
+
+//  --------------------------------------- Awal Main Program -----------------------------------------------
+        
+        public static void Main(string[] args)
         {
             
-
-Mahasiswa[] a2 = new Mahasiswa[3];
+        	Program p = new Program();
 			
-			
-			string pilih;
-			int masukan;
-			menu :
-			Console.Clear();
-            Console.WriteLine("      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-            Console.WriteLine("      |               	PROGRAM DAFTAR ABSEN MAHASISWA                       |");
-            Console.WriteLine("      |                    	KELAS D4SM-44-03                             |");
-            Console.WriteLine("      |           KELOMPOK :	1. MUHAMMAD TAUFIQ SHIDIQ                    |");
-            Console.WriteLine("      |              		2. muhammad Ainur Rafiq                      |");
-            Console.WriteLine("      |              		3. Muhammad Gilang Radhitya Permana          |");
-            Console.WriteLine("      |              			                                     |");
-            Console.WriteLine("      |              Ketikan Angk Untuk Memilih Salah 1 Menu                 |");
-            Console.WriteLine("      |_____________________________________________________________________ |");
-            Console.WriteLine("      | 1. Menambahkan Data Mahasiswa (max 30 anak)   	                     |");
-            Console.WriteLine("      | 2. Menampilkan Data Mahasiswa      				     |");
-            Console.WriteLine("      | 3. Mencari Data Mahasiswa      				             |");
-            Console.WriteLine("      | 4. Mendelete Data Mahasiswa     			             |");
-            Console.WriteLine("      |>>>>>>>>>>>>>>>>>>>>>>>>>>>  0.Keluar  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<|");
-			
-            
-    Console.Write("\nInsert Number Of Menu : ");
-    pilih = Console.ReadLine();
-    masukan = Convert.ToInt16(pilih);
-    switch(masukan)
-    {
-        case 1:
-            int u;
-
-            Console.WriteLine("jumlah anggota yang akan diinput");
-            u = int.Parse(Console.ReadLine());
-
-            for(int i=0; i <= u-1; i++){
+        	int masukan;
                 
-            Console.WriteLine("Tuliskan Absen ke ");//Menampilkan isian user
-            a2[i].Absen =  (Console.ReadLine()); //Inisialisasi data dari user ke dalam array dengan indeks tertentu
-            Console.WriteLine("Tuliskan Nama Mahasiswa ");//Menampilkan isian user
-            a2[i].Nama = (Console.ReadLine()); //Inisialisasi data dari user ke dalam array dengan indeks tertentu
-            Console.WriteLine("Tuliskan NIM ");
-            a2[i].NIM = (Console.ReadLine()); //Inisialisasi data dari user ke dalam array dengan indeks tertentu
-            }
+                menu :
+                Console.Clear();
+                Console.WriteLine("      ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                Console.WriteLine("      |               	PROGRAM DAFTAR ABSEN MAHASISWA                       |");
+                Console.WriteLine("      |                    	KELAS D4SM-44-03                             |");
+                Console.WriteLine("      |           KELOMPOK :	1. MUHAMMAD TAUFIQ SHIDIQ                    |");
+                Console.WriteLine("      |              		2. muhammad Ainur Rafiq                      |");
+                Console.WriteLine("      |              		3. Muhammad Gilang Radhitya Permana          |");
+                Console.WriteLine("      |              			                                     |");
+                Console.WriteLine("      |              Ketikan Angk Untuk Memilih Salah 1 Menu                 |");
+                Console.WriteLine("      |_____________________________________________________________________ |");
+                Console.WriteLine("      | 1. Menambahkan Data Mahasiswa                 	                     |");
+                Console.WriteLine("      | 2. Menampilkan Data Mahasiswa      				     |");
+                Console.WriteLine("      | 3. Mencari Data Mahasiswa      				             |");
+                Console.WriteLine("      | 4. Menghapus Data Mahasiswa     			             |");
+                Console.WriteLine("      |>>>>>>>>>>>>>>>>>>>>>>>>  0.Exit or Menu  <<<<<<<<<<<<<<<<<<<<<<<<<<<<|");
+                
+        Console.Write("\nInsert Number Of Menu : ");
+        masukan = Convert.ToInt32(Console.ReadLine());
+
+        
+        switch(masukan)
+        {
+            case 1:
+                p.Tambah_Mahasiswa();
+                goto menu;
+                
+            case 2:
+                p.Tampil_Mahasiswa();
+                goto menu;
+                
+            case 3:
+                p.Cari_Mahasiswa();
+                goto menu;
+                
+            case 4:
+                p.Hapus_Mahasiswa();
+                goto menu;
+                
+            case 0:
+                
+                Benerin:
+                
+	            Console.Write("are you sure want to quit? -> yes or no : ");
+	            String jawaban = Console.ReadLine();
+	            
+	            if(jawaban == "no"){
+	                goto menu;
+	            } else if (jawaban == "yes") {
+	                Console.WriteLine("-------------------------****-------------------------");
+	            	Console.WriteLine("");
+	                Console.WriteLine("************DO YOUR BEST AND THANK YOU***************");
+	                Console.WriteLine("");
+	                Console.WriteLine("-------------------------****-------------------------");
+	                Console.WriteLine("");
+	            } else if (jawaban != "yes" || jawaban != "no"){
+	            	Console.WriteLine("");
+	                Console.WriteLine("please choose 'yes' or 'no'");
+	                Console.WriteLine("");
+	                Console.WriteLine("-------------****---------------");
+	                Console.WriteLine("");
+	                Console.WriteLine("");
+	                Console.WriteLine("");
+	                goto Benerin;
+	            }	
+	            
+	            Console.WriteLine("\nPress Any Key To Exit");
+	            Console.ReadKey();
+	            break;
+			
+            
+			default:
+	            
+	            if(masukan > 4){
+	                Console.WriteLine("please input great number from 0-4");
+	                Console.WriteLine("\nPress Any Key To Select Other Menu");
+	            }
+	                    Console.ReadKey();
+	            		goto menu;
+	        } 
+        }
+        
+//  --------------------------------------- Akhir Main Program -----------------------------------------------
+        
+
+
+//  --------------------------------------- awal function tambah anggota -------------------------------------
+
+        public void Tambah_Mahasiswa(){
+
+            Console.Write("Berapa jumlah anggota yang akan anda input : ");
+            int j = urutan + Convert.ToInt32(Console.ReadLine());
+            
+            input += j;
 
             
-            
-            goto menu;
+            for(int i = urutan ; i <  j;  i++){
+            	
+                Console.WriteLine("-------------------------------------------");
+            	Console.Write("no absen anggota ke : " );
+                MyGlobal[i].Id = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Nama Anggota : ");
+                MyGlobal[i].Nama = Console.ReadLine();
+                Console.Write("Jenis Kelamin : " );
+                MyGlobal[i].Kelamin = Console.ReadLine();
+                Console.WriteLine("-------------------------------------------");
 
-        case 2:
+            urutan ++;         		
 
-            for(int i=0; i <= a2.Length; i++){
-            Console.WriteLine(" Nomor absen mahasiswa ke " +a2[i].Absen);
-            Console.WriteLine(" Nomor absen mahasiswa ke " +a2[i].Nama);
-            Console.WriteLine(" Nomor absen mahasiswa ke " +a2[i].NIM);
-            }
-            Console.WriteLine("\n    Succesfully Saved ! \nPress Any Key To Select Other Menu");
+         	} //akhir mother for
+
+            Console.WriteLine("\nPress Any Key To Select Other Menu");
             Console.ReadKey();
-            goto menu;
+            
 
-        case 0:
-        break;
-    } 
+            
+        }
+        
+//  --------------------------------------- akhir function tambah anggota -------------------------------------
+
+
+
+//  --------------------------------------- awal function menampilkan anggota --------------------------------- 
+
+            public void Tampil_Mahasiswa(){
+
+				for(int i = 1; i < input; i++){
+					Console.WriteLine("-------------------------------------------");
+            		Console.WriteLine(" Nomor absen mahasiswa ke : " + MyGlobal[i].Id);
+            		Console.WriteLine(" Namaa anggota : " + MyGlobal[i].Nama);
+            		Console.WriteLine(" Jenis Kelamin : " + MyGlobal[i].Kelamin);
+            		Console.WriteLine("-------------------------------------------");
+            		Console.WriteLine(input);
+				}
+
+				Console.WriteLine("\nPress Any Key To Select Other Menu");
+				Console.ReadKey();
 
         }
+        
+//   --------------------------------------- akhir function menampilkan anggota ---------------------------------
+		
+		
+		
+//	 --------------------------------------- awal function menampilkan anggota --------------------------------- 
+
+            public void Cari_Mahasiswa(){
+	
+					int cari;
+					int ketemu = 0, posisi = 0;
+	
+				Console.Write("\n\nNomor absen mahasiswa yang akan dicari = ");
+				cari = Convert.ToInt32(Console.ReadLine());
+
+				for (int n = 0; n <= input; n++){
+				    if (cari == MyGlobal[n].Id){
+				    ketemu = 1;
+				    posisi = n;
+				
+				    
+				    Console.WriteLine(" mahasiswa dengan no absen ke {0} Ditemukan", cari);
+            		Console.WriteLine(" Nama Mahasiswa : " + MyGlobal[cari].Nama);
+            		Console.WriteLine(" Jenis Kelamin : " + MyGlobal[cari].Kelamin);
+				    }
+				}
+				
+				if (ketemu == 0){
+				    Console.WriteLine("Maaf Data yang kamu cari tidak ada.");
+				}
+				
+				Console.WriteLine("\nPress Any Key To Select Other Menu");
+				Console.ReadKey();
+
+        }
+        
+//   --------------------------------------- akhir function menampilkan anggota ---------------------------------
+
+
+
+//	 --------------------------------------- awal function menampilkan anggota --------------------------------- 
+
+            public void Hapus_Mahasiswa(){
+	
+				int hapus;
+					
+				Console.Write("\n\nMasukkan index data yang akan dihapus = ");
+                hapus = Convert.ToInt32(Console.ReadLine());
+                for (int p = hapus; p < input; p++)
+                {
+                    MyGlobal[p].Id = MyGlobal[p + 1].Id;
+                    input -= 1;
+                }
+                
+                for (int p = 1; p < input; p++)
+                {
+                    Console.WriteLine("your id is : {0} ", MyGlobal[p].Id);
+                }
+					Console.ReadKey();
+                
+        }
+        
+//   --------------------------------------- akhir function menampilkan anggota ---------------------------------
+
+
     }
 }
